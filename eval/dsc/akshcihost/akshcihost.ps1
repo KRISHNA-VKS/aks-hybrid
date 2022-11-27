7abaa9b3ad427616413a59b2ab4f1267c94c06bd
KRISHNA SANKAR V <krishna.sankar@siemens-healthineers.com>
2022-11-27 18:53:15 +0530

    update specific version1

--- eval/dsc/akshcihost/akshcihost.ps1
+++ eval/dsc/akshcihost/akshcihost.ps1
@@ -20,7 +20,7 @@ configuration AKSHCIHost
     ) 
     
     Import-DscResource -ModuleName 'PSDesiredStateConfiguration'
-    Import-DscResource -ModuleName 'xPSDesiredStateConfiguration'
+    Import-DscResource -ModuleName 'xPSDesiredStateConfiguration' -ModuleVersion '9.1.0'
     Import-DscResource -ModuleName 'ComputerManagementDsc'
     Import-DscResource -ModuleName 'xHyper-v'
     Import-DscResource -ModuleName 'StorageDSC'
